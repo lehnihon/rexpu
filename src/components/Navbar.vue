e1c649c7b95639be37b9fea608a365b730633e7b
Leandro <lehnihon@hotmail.com>
2019-04-04 19:32:19 -0300

    Base Menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
     <v-toolbar flat app>
-      <v-toolbar-side-icon class="grey--text" @click="drawer= !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon class="grey--text" @click="mini= !mini"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
-        <span class="font-weight-light">Todo</span>
-        <span>Ninja</span>
+        <span class="font-weight-light">Win</span>
+        <span>Up</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat color="grey">
@@ -12,8 +12,28 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
-    <v-navigation-drawer fixed app v-model="drawer" class="indigo">
-      <p>teste</p>
+    <v-navigation-drawer :mini-variant.sync="mini" fixed app class="primary">
+      <v-list>
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
+          <v-list-tile-content class="white--text">
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-divider class="deep-purple mx-2"></v-divider>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+              <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -22,7 +42,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      mini: true,
+      links:[
+          { icon: 'dashboard', text: 'Dashboard', route: '/' },
+          { icon: 'folder', text: 'My Projects', route: '/projects' },
+          { icon: 'person', text: 'Team', route: '/team' }
+      ]
     };
   }
 };
---
+++ src/views/Team.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="team">
+    <h1 class="subheading grey--text mx-4">Team</h1>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12>
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    components: {
+    }
+  }
+</script>
